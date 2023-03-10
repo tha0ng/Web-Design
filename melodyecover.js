aab876f18cd6a5ff4b8ba8d762647df16c687cda
tha0ng <hathao.nguyen202@gmail.com>
2023-03-10 06:30:58 -0500

    update

--- melodyecover.js
+++ melodyecover.js
@@ -1,12 +1,6 @@
 
 
-//custom cursor
-const cursorFollower = document.querySelector('.cursor-follower');
 
-document.addEventListener('mousemove', e => {
-  cursorFollower.style.left = e.pageX + 'px';
-  cursorFollower.style.top = e.pageY + 'px';
-});
 
 
 
